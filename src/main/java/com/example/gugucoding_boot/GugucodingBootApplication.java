ddbaa65a080bb51e10f38f5396f625524b3f6fc7
ShinJiHyeong <bs9992@nara.co.kr>
2021-06-17 11:37:53 +0900

    Initial commit

---
+++ src/main/java/com/example/gugucoding_boot/GugucodingBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gugucoding_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GugucodingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GugucodingBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gugucoding_boot/GugucodingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gugucoding_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GugucodingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
