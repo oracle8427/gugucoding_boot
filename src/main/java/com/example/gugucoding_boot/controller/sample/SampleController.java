775e486149bd61f7881e7674e3307053a17993c9
ShinJiHyeong <bs9992@nara.co.kr>
2021-06-23 11:00:01 +0900

    Thymeleaf th:inline="javascript"
    JavaScript 처리에 유용
    모델을 <script> 태그내에서 JavaScript 와 함께 사용할 수 있도록
    해준다.

--- src/main/java/com/example/gugucoding_boot/controller/sample/SampleController.java
+++ src/main/java/com/example/gugucoding_boot/controller/sample/SampleController.java
@@ -4,12 +4,15 @@ import com.example.gugucoding_boot.entity.sample.SampleDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 @Controller
@@ -26,4 +29,32 @@ public class SampleController {
 						.registrationDateTime(LocalDateTime.now()).build()).collect(Collectors.toList());
 		model.addAttribute("sampleModels", sampleModels);
 	}
+
+	@GetMapping("model")
+	public String sampleModel(@RequestParam long id, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("id", id);
+		return "redirect:/sample/inline";
+	}
+
+	@GetMapping("model/{id}")
+	public String sampleModel2(@PathVariable long id, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("id", id);
+		return "redirect:/sample/inline";
+	}
+
+	@GetMapping("inline")
+	public void inlineJS(Model model, @RequestParam(value = "id", required = false) Long id) {
+		if (id == null) {
+			Random random = new Random();
+			id = (long) (random.nextInt(30) + 1);
+		}
+
+		SampleDTO sampleModel = SampleDTO.builder()
+				.id(id)
+				.first("[first]Inline JS Test...")
+				.last("[last]Inline JS Test...")
+				.registrationDateTime(LocalDateTime.now()).build();
+		model.addAttribute("sampleModel", sampleModel);
+	}
+
 }
